a712668d59c1687e3e0ab662984851e93b57426c
JeongHojin96 <JeongHojin96@JeongHojin96.com>
2023-04-13 16:41:45 +0900

    first

---
+++ hello_01/src/com/callor/hello/data/HelloYour.java
@@ -0,0 +1,15 @@
+package com.callor.hello.data;
+
+/*
+ * Source Code, Java Class File
+ * 주석, Comment
+ */
+public class HelloYour {
+	public static void main(String[] args) {
+		System.out.println(30 + 40);
+		System.out.println(30 - 40);
+		System.out.println(30 * 40);
+		System.out.println(30 / 40);
+		System.out.println(30 % 40);
+	}
+}
